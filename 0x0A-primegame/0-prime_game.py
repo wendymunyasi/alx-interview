9bc885dddf0893d4b612bdf1f539e27c7a268873
wendy munyasi <wendymunyasi@gmail.com>
2023-03-09 13:46:50 +0300

    Add comments to code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,27 +3,47 @@
 
 
 def isWinner(x, nums):
-    """x - rounds
-    nums - numbers list
     """
+    Determines the winner of a set of prime number removal games.
+
+    Args:
+        x (int): The number of rounds.
+        nums (list of int): A list of integers where each integer n denotes
+        a set of consecutive integers starting from 1 up to and including n.
+
+    Returns:
+        str: The name of the player who won the most rounds (either "Ben"
+        or "Maria").
+        None: If the winner cannot be determined.
+
+    Raises:
+        None.
+    """
+    # Check for invalid input
     if x <= 0 or nums is None:
         return None
     if x != len(nums):
         return None
-
+    # Initialize scores and array of possible prime numbers
     ben = 0
     maria = 0
-
+    # Create a list 'a' of length sorted(nums)[-1] + 1 with all elements
+    # initialized to 1
     a = [1 for x in range(sorted(nums)[-1] + 1)]
+    # The first two elements of the list, a[0] and a[1], are set to 0
+    # because 0 and 1 are not prime numbers
     a[0], a[1] = 0, 0
+    # Use Sieve of Eratosthenes algorithm to generate array of prime numbers
     for i in range(2, len(a)):
         rm_multiples(a, i)
-
+    # Play each round of the game
     for i in nums:
+        # If the sum of prime numbers in the set is even, Ben wins
         if sum(a[0:i + 1]) % 2 == 0:
             ben += 1
         else:
             maria += 1
+    # Determine the winner of the game
     if ben > maria:
         return "Ben"
     if maria > ben:
@@ -32,9 +52,26 @@ def isWinner(x, nums):
 
 
 def rm_multiples(ls, x):
-    """removes multiple
-    of primes
     """
+    Removes multiples of a prime number from an array of possible prime
+    numbers.
+
+    Args:
+        ls (list of int): An array of possible prime numbers.
+        x (int): The prime number to remove multiples of.
+
+    Returns:
+        None.
+
+    Raises:
+        None.
+    """
+    # This loop iterates over multiples of a prime number and marks them as
+    # non-prime by setting their corresponding value to 0 in the input
+    # list ls. Starting from 2, it sets every multiple of x up to the
+    # length of ls to 0. If the index i * x is out of range for the list ls,
+    # the try block will raise an IndexError exception, and the loop will
+    # terminate using the break statement.
     for i in range(2, len(ls)):
         try:
             ls[i * x] = 0
